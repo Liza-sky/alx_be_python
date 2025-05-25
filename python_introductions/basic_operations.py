30470ad3431fc7e89c1ab0744034e5e45940d5b1
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-05-25 22:31:26 +0300

    updated basic_operations.py

---
+++ python_introductions/basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+
+number2 = 5 
+
+sum_result = number1 + number2
+
+print(sum_result) 
+
+subtraction_result = number1 - number2
+
+print(subtraction_result)
+
+multipy_result = number1 * number2
+
+print(multipy_result)

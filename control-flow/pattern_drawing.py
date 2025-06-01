b4fdbc8ed8a85f1dfccdc597086a288685c6c0b6
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-06-02 00:11:51 +0300

    update pattern_ drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,10 @@
-def print_square_pattern(n):
-    for i in range(n):
-        for j in range(n):
+size = int(input("Enter the size of the pattern: "))
+
+row = 0
+
+
+    while row < size:
+        for _ in range(n):
             print("*", end="")
             print() # print a newline after each row
+            row+= 1

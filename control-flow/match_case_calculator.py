84198e6518453e3706258b3d4c994a1bd6170860
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-06-01 23:36:39 +0300

    file multiplication_table.py and updated match_case_calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+first = float(input("Enter the first number: "))
+second = float(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case "+":
+        print(f"The result is {first + second}.")
+    case "-":
+        print(f"The result is {first - second}.")
+    case "*":
+        print(f"The result is {first * second}.")
+    case "/":
+        if second == 0:
+            print("Cannot divide by zero.")
+        else:
+            print(f"The result is {first / second}.")
+    case _:
+        print("Invalid operation.")
+        

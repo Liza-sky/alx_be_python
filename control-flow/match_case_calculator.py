817c054eba4c041ea43dfc2ab1c07ec7b5f4131c
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-06-02 00:05:21 +0300

    updated match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,19 +1,18 @@
-first = float(input("Enter the first number: "))
-second = float(input("Enter the second number: "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 
 match operation:
     case "+":
-        print(f"The result is {first + second}.")
+        print(f"The result is {num1 + num2}.")
     case "-":
-        print(f"The result is {first - second}.")
+        print(f"The result is {num1 - num2}.")
     case "*":
-        print(f"The result is {first * second}.")
+        print(f"The result is {num1 * num2}.")
     case "/":
-        if second == 0:
+        if num2 == 0:
             print("Cannot divide by zero.")
         else:
-            print(f"The result is {first / second}.")
+            print(f"The result is {num1 / num2}.")
     case _:
         print("Invalid operation.")
-        
--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -3,3 +3,4 @@ age_in_2050 = current_age + 27
 
 print(f"In 2050, you will be {age_in_2050} years old")
 
+

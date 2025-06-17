99a9781c948f87be0b08d9327226cc5a675d2be9
Liza-sky <119234355+Liza-sky@users.noreply.github.com>
2025-06-17 16:26:30 +0300

    test-simple_calculator.py file

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,45 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+
+class TestSimpleCalculator(unittest.TestCase):
+
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+
+    def test_addition(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-5, -3), -8)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(1.5, 2.5), 4.0)
+
+
+    def test_substraction(self):
+        self.assertEqual(self.calc.add(5, 3), 2)
+        self.assertEqual(self.calc.add(3, 5), -2)
+        self.assertEqual(self.calc.add(-5, -3), -2)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(2.5, 1.5), 1.0)
+
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.add(4, 3), 12)
+        self.assertEqual(self.calc.add(-2, 5), -10)
+        self.assertEqual(self.calc.add(0, 100), 0)
+        self.assertEqual(self.calc.add(1.5, 2), 3.0)
+        self.assertEqual(self.calc.add(-1.5, -2), 3,0)
+
+
+    def test_division(self):
+        self.assertEqual(self.calc.add(10, 2), 5)
+        self.assertEqual(self.calc.add(-6, 3), -2)
+        self.assertEqual(self.calc.add(7.5, 2.5), 3.0)
+        self.assertEqual(self.calc.add(10, 0))
+        self.assertEqual(self.calc.add(0, 0))
+        self.assertEqual(self.calc.add(0, 5), 0)
+
+if __name__ == '__main__':
+    unittest.main()
